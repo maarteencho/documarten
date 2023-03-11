e0fd130f280bf80d50d4705d219bae8876fa5835
Martin T <martin.tenev@gmail.com>
2023-03-11 22:37:13 +0000

    Add actions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "My Sites",
-  tagline: "Dinosaurs are cools",
+  tagline: "Dino",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
