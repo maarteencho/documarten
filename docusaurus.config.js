fbd7f6d6cc940f7b874b90d51088b4f724c71e90
Martin T <martin.tenev@gmail.com>
2023-03-11 23:26:33 +0000

    Typo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "My Sites",
-  tagline: "Dinos and cats",
+  tagline: "Dinos and cats!",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
