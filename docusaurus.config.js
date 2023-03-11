27efba4200572648efedb06cb76f74073c25a9b4
Martin T <martin.tenev@gmail.com>
2023-03-11 22:50:12 +0000

    Yarn.lock

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "My Sites",
-  tagline: "Dinos",
+  tagline: "Dinosss",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
